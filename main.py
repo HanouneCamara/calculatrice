06d594cdc46838272d7509e2fc549e2bb5a398cb
HanouneCamara <Alicmr13@gmail.com>
2023-11-25 21:12:54 +0000

    ajout_op

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ premier_nombre = int(input("Entrez un premier nombre : "))
 
 deuxieme_nombre = int(input("Entrez un deuxième nombre : "))
 
+operateur = input("Choisissez un opérateur(+ - * /) : ")
 
-print(f"Le résultat de l'addition de {premier_nombre} avec {deuxieme_nombre} est égal à {premier_nombre + deuxieme_nombre} ")
+if operateur == "+":
+    print(f"Le résultat de l'addition de {premier_nombre} avec {deuxieme_nombre} est égal à {premier_nombre + deuxieme_nombre} ")
+elif operateur == "-":
+    print(f"Le résultat de la soustraction de {premier_nombre} avec {deuxieme_nombre} est égal à {premier_nombre - deuxieme_nombre} ")
+elif operateur == "*":
+    print(f"Le résultat de la multiplication de {premier_nombre} avec {deuxieme_nombre} est égal à {premier_nombre * deuxieme_nombre} ")
+elif operateur == "/":
+    print(f"Le résultat de la division de {premier_nombre} avec {deuxieme_nombre} est égal à {premier_nombre / deuxieme_nombre} ")
+else:
+    print("Opérateur invalide")
