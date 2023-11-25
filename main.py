a4fea92995e66825335178f43add2eec6cf2a14b
HanouneCamara <Alicmr13@gmail.com>
2023-11-25 15:24:45 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+premier_nombre = int(input("Entrez un premier nombre : "))
+
+deuxieme_nombre = int(input("Entrez un deuxième nombre : "))
+
+resultat = premier_nombre + deuxieme_nombre
+
+print("Le résultat de l'addition de " + str(premier_nombre) + " avec " + str(deuxieme_nombre) + " est égal à " + str(resultat))
